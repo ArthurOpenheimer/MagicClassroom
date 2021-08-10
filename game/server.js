03fbb9ed9535facdd9ad411414531d35b0c7bf6e
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-08-09 21:08:40 -0300

    creating game scene and setting current scene to client

--- game/server.js
+++ game/server.js
@@ -1,16 +1,22 @@
 import express from "express"
 import http from "http"
 import { Server } from "socket.io"
+import createAbstractScene from "./components/abstractScene.js"
 
 const app = express()
 const httpServer = http.createServer(app)
 const sockets = new Server(httpServer);
 app.use(express.static("public"))
 
+const scene = createAbstractScene();
+
 sockets.on('connection', (socket) => {
     const playerId = socket.id;
     console.log(`Player ${playerId} connected in the server`) 
 
+    scene.addPlayer(playerId);
+
+    socket.emit('setup', {state: scene.getState(),});
 
     socket.on('disconnect', () => {
         //remove player
