32cd5dbe54e1fc88a202a842bda6e5ea78765e1b
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-08-12 13:23:25 -0300

    Adding movement to client player and communicating to server

--- game/public/assets/components/scene.js
+++ game/public/assets/components/scene.js
@@ -1,12 +1,17 @@
 import createPlayer from "./player.js";
-export default function createScene(htmlDOM, PIXI, newState) {
+import createKeyListner from "./keyboard-listner.js";
+
+export default function createScene(htmlDOM, PIXI, newState, clientId) {
+    let left, up, down, right;
+    let observers = [];
     let app;
     let textures = {};
-    const loader = PIXI.Loader.shared;
-    const ticker = PIXI.Ticker.shared;
     let state = {
         players: {},
-    };
+    }
+    const loader = PIXI.Loader.shared;
+    const ticker = PIXI.Ticker.shared;
+    const currentPlayerId = clientId;
 
     function start(element){
         app = new PIXI.Application({
@@ -16,12 +21,8 @@ export default function createScene(htmlDOM, PIXI, newState) {
             backgroundColor: 0xffffff,
             resolution: 1,
         });
-
+        PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
         loadAssets(setup);
-    }
-
-    function setup(){ 
-        setState(newState);
     }
 
     function loadAssets(setup){
@@ -30,26 +31,40 @@ export default function createScene(htmlDOM, PIXI, newState) {
         loader.load((loader, resources) => {
             textures['student'] = PIXI.Texture.from('student');
             setup();
-        })
+        });
+
         loader.onProgress.add((loader, resource) => {
             console.log(`Loading... ${loader.progress}%`)
-        })
+        });
+
         loader.onError.add((loader, resource) => {
             console.log(`An error ocurred when loading, ${resource}`)
-        })
+        });
+
+    }
+
+    function setup(){ 
+        setState(newState);
+
+        left = createKeyListner("a"),
+        up = createKeyListner("w" ),
+        right = createKeyListner("d"),
+        down = createKeyListner("s");
+        moveClientPlayer();
     }
 
     function setState(newState){
         const players = newState.players;
         for(const player in players) {
             addPlayer(players[player]);
-        };
+        }
     }
 
     function addPlayer(newPlayer){
-        const player = createPlayer(newPlayer.id);
+        const player = createPlayer(newPlayer.id, notifyAll);
 
         player.sprite = new PIXI.Sprite(textures[newPlayer.textureId]);
+        player.sprite.scale.set(3,3);
         player.velocity = newPlayer.velocity;
         player.setPosition({x: newPlayer.x, y: newPlayer.y});
         ticker.add(delta => player.loop(delta));
@@ -65,8 +80,68 @@ export default function createScene(htmlDOM, PIXI, newState) {
         delete state.players[playerId];
     }
 
+    function moveClientPlayer(){  
+        const player = state.players[currentPlayerId];
+
+        left.press = () => {
+            player.setInput({x: -1, y: player.input.y})
+        };
+        left.release = () => {
+            if(!right.isDown){
+                player.setInput({x: 0, y: player.input.y})
+            }
+        };
+
+        up.press = () => {
+            player.setInput({x: player.input.x, y: -1})
+        };
+        up.release = () => {
+            if(!down.isDown){
+                player.setInput({x: player.input.x, y: 0})
+            }
+        };
+
+        right.press = () => {
+            player.setInput({x: 1, y: player.input.y})
+        };
+        right.release = () => {
+            if(!left.isDown){
+
+                player.setInput({x: 0, y: player.input.y})
+            }
+        };
+
+        down.press = () => {
+            player.setInput({x: player.input.x, y: 1})
+        };
+        down.release = () => {
+            if(!up.isDown){
+                player.setInput({x: player.input.x, y: 0})
+            }
+        };
+    }
+
+    function moveProxy(movement) {
+        const playerId = movement.id;
+        const input = movement.input;
+        const player = state.players[playerId];
+
+        player.setPosition(movement.position);
+        player.input = input;
+    }
+
     function addOnStage(object){
         app.stage.addChild(object);
+    }
+
+    function subscribe(observerFunction) {
+        observers.push(observerFunction);
+    }
+
+    function notifyAll(command) {
+        for(const observerFunction of observers){
+            observerFunction(command);
+        }
     }
 
     start(htmlDOM);
@@ -75,5 +150,8 @@ export default function createScene(htmlDOM, PIXI, newState) {
         state,
         addPlayer,
         removePlayer,
-    };
+        moveClientPlayer,
+        moveProxy,
+        subscribe,
+    }
 }
