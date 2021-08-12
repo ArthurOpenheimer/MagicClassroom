49c98ad933d0cb43fb2fa974c014fd8ed45eeefc
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-08-12 13:23:50 -0300

    Fixing diagonal move

--- game/public/assets/components/player.js
+++ game/public/assets/components/player.js
@@ -1,4 +1,4 @@
-export default function createPlayer(playerId) {
+export default function createPlayer(playerId, notifyAll) {
 
     let player = {
         sprite: null,
@@ -6,22 +6,37 @@ export default function createPlayer(playerId) {
         velocity: 5,
         input: {
             x: 0,
-            y: 0
+            y: 0,
         },
         
-        move(delta){
-            this.sprite.x += this.input.x * this.velocity * delta;
-            this.sprite.y += this.input.y * this.velocity * delta;
+        move(delta) {
+            let diagonalAjust = 1;
+            if(this.input.x !=   0 && this.input.y != 0){
+                diagonalAjust = (Math.sqrt(2)/2);
+            }
+            this.sprite.x += this.input.x * this.velocity * delta * diagonalAjust;
+            this.sprite.y += this.input.y * this.velocity * delta * diagonalAjust;
         },
 
-        setPosition(position){
+        setPosition(position) {
             if(!position.x && !position.y) return 
 
             this.sprite.x = position.x;
             this.sprite.y = position.y;
         },
-        
-        getPosition(){
+
+        setInput(input){
+            if(input == this.input) return;
+            this.input = input;
+            notifyAll({
+                type: 'move-player',
+                input: this.input,
+                position: this.getPosition(),
+                id: this.id,
+            });
+        },
+
+        getPosition() {
             return {
                 x: this.sprite.x,
                 y: this.sprite.y,
