1a4af1c2a2f1493995b9aceff7dc045e573b878d
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-08-12 13:57:23 -0300

    recovering file

---
+++ game/server.js
@@ -0,0 +1,38 @@
+import express from "express"
+import http from "http"
+import { Server } from "socket.io"
+import createAbstractScene from "./components/abstractScene.js"
+
+const app = express()
+const httpServer = http.createServer(app)
+const sockets = new Server(httpServer);
+app.use(express.static("public"))
+
+const scene = createAbstractScene();
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id;
+    console.log(`Player ${playerId} connected in the server`) ;
+
+    scene.addPlayer(playerId);
+
+    socket.emit('setup', {state: scene.getState()});
+
+    scene.subscribe((command) => {
+        socket.emit(command.type, command);
+    });
+
+    socket.on('move-player', (command) => {
+        scene.movePlayer(command);
+    });
+
+    socket.on('disconnect', () => {
+        scene.removePlayer(playerId);
+        console.log(`Player ${playerId} disconnected from the server`)
+    });
+
+})
+
+httpServer.listen(3000, () => {
+    console.log("Server listening on port: 3000")
+})   
