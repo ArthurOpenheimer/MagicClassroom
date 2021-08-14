2c82f185410763ac157f6bdcde127d8b72b5cb4a
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-08-14 17:21:15 -0300

    Creating animations structure and making player a container

--- game/public/assets/components/player.js
+++ game/public/assets/components/player.js
@@ -1,7 +1,25 @@
-export default function createPlayer(playerId, notifyAll) {
+export default function createPlayer(playerId, notifyAll, PIXI, sheet) {
 
     let player = {
-        sprite: null,
+        body: null,
+        currentAnimation: null,
+        animations: {
+            idle: {
+                up: null,
+                right: null,
+                down: null,
+                left: null
+            },
+            walk: {
+                up: null,
+                right: null,
+                down: null,
+                left: null
+            }
+        },
+
+        _facing: null,
+        spriteId: null,
         id: playerId,
         velocity: 5,
         input: {
@@ -14,20 +32,63 @@ export default function createPlayer(playerId, notifyAll) {
             if(this.input.x !=   0 && this.input.y != 0){
                 diagonalAjust = (Math.sqrt(2)/2);
             }
-            this.sprite.x += this.input.x * this.velocity * delta * diagonalAjust;
-            this.sprite.y += this.input.y * this.velocity * delta * diagonalAjust;
+            this.body.x += this.input.x * this.velocity * delta * diagonalAjust;
+            this.body.y += this.input.y * this.velocity * delta * diagonalAjust;
+        },
+
+        setAnimation() {
+            let newAnimation;
+            if(this.input.x == 0) {
+                newAnimation = this.animations.idle[this._facing];
+                
+            }
+            else{
+                newAnimation = this.animations.walk[this._facing];
+            }
+
+            if(!newAnimation || newAnimation == this.currentAnimation) return
+
+            if(this.currentAnimation) {
+                this.body.removeChild(this.currentAnimation);
+                this.currentAnimation.stop();   
+            } 
+
+            newAnimation.play();
+            newAnimation.animationSpeed = 0.1;
+            this.body.addChild(newAnimation);
+
+            this.currentAnimation = newAnimation;
         },
 
         setPosition(position) {
             if(!position.x && !position.y) return 
 
-            this.sprite.x = position.x;
-            this.sprite.y = position.y;
+            this.body.x = position.x;
+            this.body.y = position.y;
+        },
+
+        setSprite(spriteId) {
+            this.spriteId = spriteId;
+        },
+        
+        setFacing(direction) {
+            this._facing = direction;
+            this.setAnimation();
+        },
+
+        getFacing() {
+            return this._facing;
         },
 
         setInputX(x){
             if(x == this.input.x) return;
             this.input.x = x;
+
+            if(x > 0) this.setFacing("right");
+            else if(x < 0) this.setFacing("left");
+            //Set idle animation
+            else this.setAnimation()
+
             notifyAll({
                 type: 'move-player',
                 input: this.input,
@@ -35,9 +96,16 @@ export default function createPlayer(playerId, notifyAll) {
                 id: this.id,
             });
         },
+
         setInputY(y){
             if(y == this.input.y) return;
             this.input.y = y;
+            
+            if(y > 0) this.setFacing("down");
+            else if(y < 0) this.setFacing("up");
+            //Set idle animation
+            else this.setAnimation()
+
             notifyAll({
                 type: 'move-player',
                 input: this.input,
@@ -48,8 +116,8 @@ export default function createPlayer(playerId, notifyAll) {
 
         getPosition() {
             return {
-                x: this.sprite.x,
-                y: this.sprite.y,
+                x: this.body.x,
+                y: this.body.y,
             };
         },
 
@@ -57,7 +125,18 @@ export default function createPlayer(playerId, notifyAll) {
             this.move(delta);
             //checkCollision();
         },
-    }        
+    };      
 
+    function setConfig() {
+        player.animations.idle.down = new PIXI.AnimatedSprite(sheet.animations["student01IdleDown"]);
+        player.animations.idle.right = new PIXI.AnimatedSprite(sheet.animations["student01IdleRight"]);
+        player.animations.idle.left = new PIXI.AnimatedSprite(sheet.animations["student01IdleLeft"]);
+        player.animations.walk.right = new PIXI.AnimatedSprite(sheet.animations["student01WalkingRight"]);
+        player.animations.walk.left = new PIXI.AnimatedSprite(sheet.animations["student01WalkingLeft"]);
+        player.body = new PIXI.Container();
+        player.body.scale.set(3,3);
+    }
+
+    setConfig()
     return player;
 }
