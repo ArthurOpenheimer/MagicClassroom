0b3d5d4583764aea1f528a7c4b538c09284e9042
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-08-14 17:20:34 -0300

    Using SpriteId instead of TextureId

--- game/components/abstractScene.js
+++ game/components/abstractScene.js
@@ -11,7 +11,7 @@ export default function createAbstractScene(){
 
     function addPlayer(playerId){
         const player = createAbstractPlayer(playerId);
-        player.textureId = "student";
+        player.spriteId = "student01";
         player.setPosition({
             x: Math.floor(Math.random() * 400 + 100),
             y: Math.floor(Math.random() * 400 + 100),
