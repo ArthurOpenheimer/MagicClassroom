296cd0c444fe0490befabc05345245de0011a848
Eduardo Valias <edupv123@hotmail.com>
2021-10-10 17:37:06 -0300

    Adding subject 2 code

--- site/magic-moodle/src/app/tmat/tmat.component.ts
+++ site/magic-moodle/src/app/tmat/tmat.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tmat.component.css']
 })
 export class TmatComponent implements OnInit {
+  wasPressed = false;
+  sent = false;
 
+  click(state: any) {
+    this.wasPressed = state;
+    this.sent = false;
+  }
+
+  send() {
+    this.sent = true;
+  }
   constructor() { }
 
   ngOnInit(): void {
