9ca8337f35dbe37687ee1ca6ae94435f4d30badd
Eduardo Valias <edupv123@hotmail.com>
2021-10-10 17:40:31 -0300

    Adding subject 4 code

--- site/magic-moodle/src/app/tsd/tsd.component.ts
+++ site/magic-moodle/src/app/tsd/tsd.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tsd.component.css']
 })
 export class TsdComponent implements OnInit {
+  wasPressed = false;
+  sent = false;
 
+  click(state: any) {
+    this.wasPressed = state;
+    this.sent = false;
+  }
+
+  send() {
+    this.sent = true;
+  }
   constructor() { }
 
   ngOnInit(): void {
