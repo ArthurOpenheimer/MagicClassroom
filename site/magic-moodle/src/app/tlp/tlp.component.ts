a80819ceb4a7d9c2c274f03f813eaacd3e5beb22
Eduardo Valias <edupv123@hotmail.com>
2021-10-10 17:34:38 -0300

    Adding subject 1 code

--- site/magic-moodle/src/app/tlp/tlp.component.ts
+++ site/magic-moodle/src/app/tlp/tlp.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tlp.component.css']
 })
 export class TlpComponent implements OnInit {
+  wasPressed = false;
+  sent = false;
 
+  click(state: any) {
+    this.wasPressed = state;
+    this.sent = false;
+  }
+
+  send() {
+    this.sent = true;
+  }
   constructor() { }
 
   ngOnInit(): void {
